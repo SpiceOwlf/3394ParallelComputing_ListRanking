e921377d4cb4c6161e311e8b90cebbbf88d8f911
SpicyOwl <duanxu@bc.edu>
2019-04-21 12:19:51 -0400

    some changes

--- listRankingLinear.c
+++ listRankingLinear.c
@@ -21,62 +21,92 @@ int main ()
    // printArray(res,arrayLen);
    int *updateResult;
    updateResult = updateRes(res, successor, arrayLen);
-   printArray(updateResult,arrayLen);
+   // printArray(updateResult,arrayLen);
 /////////////////////////////////////////
 // independent set here;
 //everything in U is 0 initially;
-    // int **u = (int**) malloc(sizeof(int*) * arrayLen);
-    // for(int i = 0; i < arrayLen; i++){
-    //   u[i] = (int*) malloc(sizeof(int*) * 3);
-    // }
-    // for(int j = 0; j< arrayLen; j++){
-    //   for(int k = 0; k< 3; k++){
-    //     u[j][k] = 0;
-    //   }
-    // }
+  int **u = create2Darray(10);
+//print 2D array
+    for(int i = 0; i< 10; i++){
+      for(int j = 0; j<3; j++){
+        printf("%d ",u[i][j] );
+      }
+      printf("\n" );
+    }
+///////////////////////////////////////
+printf("res: " );
+printArray(res, arrayLen);
+printf("successor: " );
+printArray(successor, arrayLen);
+printf("predecessor: " );
+printArray(predecessor, arrayLen);
+int *n_list = setValueForN(arrayLen);
+printArray(n_list, arrayLen);
 
-// print func here
-    // for(int i = 0; i< 10; i++){
-    //   for(int j = 0; j<3; j++){
-    //     printf("%d ",(u[i])[j] );
-    //   }
-    //   printf("\n" );
-    // }
-// /////////////////////////////////////////
+// for(int i = 1; i< 10; i++){
+// if(n_list[i] != 0){
+//   u[n_list[i]][0] = i;
+//   u[n_list[i]][1] = successor[i];
+//   u[n_list[i]][2] = res[i];
+// }
+// res[predecessor[l]] = res[predecessor[l]] + res[l];
+// successor[predecessor[l]] = successor[l];
+// predecessor[successor[l]] = predecessor[l];
 //
-// printArray(res, arrayLen);
-// printArray(successor, arrayLen);
-// printArray(predecessor, arrayLen);
-//     int N[arrayLen];
-//     for(int m = 0; m< arrayLen;m++){
-//       N[m] = 0;
-//     }
-//
-//     N[1] = 1;
-//     N[5] = 2;
-//     N[6] = 3;
-//     for(int l = 1; l< 10;l++){
-//     if(N[l] != 0){
-//       u[N[l]][0] = l;
-//       u[N[l]][1] = successor[l];
-//       u[N[l]][2] = res[l];
-//     }
-//     res[predecessor[l]] = res[predecessor[l]] + res[l];
-//     successor[predecessor[l]] = successor[l];
-//     predecessor[successor[l]] = predecessor[l];
-//   }
-//   printf("1111111111111111111\n" );
-//   printArray(res, arrayLen);
-//   printArray(successor, arrayLen);
-//   printArray(predecessor, arrayLen);
-//
-//     for(int i = 0; i< 10; i++){
-//       for(int j = 0; j<3; j++){
-//         printf("%d ",(u[i])[j] );
-//       }
-//       printf("\n" );
-//     }
-// //res,successor; predecessor
+// }
+
+  int i = 1;
+  u[n_list[i]][0] = i;
+  u[n_list[i]][1] = successor[i];
+  u[n_list[i]][2] = res[i];
+
+res[predecessor[i]] = res[predecessor[i]] + res[i];
+successor[predecessor[i]] = successor[i];
+predecessor[successor[i]] = predecessor[i];
+printf("1111111111111111111\n" );
+
+
+for(int i = 0; i< 10; i++){
+for(int j = 0; j<3; j++){
+  printf("%d ",u[i][j] );
+}
+printf("\n" );
+}
+printf("res: " );
+printArray(res, arrayLen);
+printf("successor: " );
+printArray(successor, arrayLen);
+printf("predecessor: " );
+printArray(predecessor, arrayLen);
+
+
+i=3;
+u[n_list[i]][0] = i;
+u[n_list[i]][1] = successor[i];
+u[n_list[i]][2] = res[i];
+
+res[predecessor[i]] = res[predecessor[i]] + res[i];
+successor[predecessor[i]] = successor[i];
+predecessor[successor[i]] = predecessor[i];
+  printf("1111111111111111111\n" );
+
+
+for(int i = 0; i< 10; i++){
+  for(int j = 0; j<3; j++){
+    printf("%d ",u[i][j] );
+  }
+  printf("\n" );
+}
+printf("res: " );
+printArray(res, arrayLen);
+printf("successor: " );
+printArray(successor, arrayLen);
+printf("predecessor: " );
+printArray(predecessor, arrayLen);
+
+
+
+//// res,successor; predecessor
 
 
 
